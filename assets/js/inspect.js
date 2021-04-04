cc2c3316a177ef0dcb0ac08e7fa74e7475a32d30
Vibhanshu Jaiswal <60248401+vibhj@users.noreply.github.com>
2021-04-04 22:59:57 +0530

    Delete inspect.js

--- assets/js/inspect.js
+++
@@ -1,8 +0,0 @@
-
-			var message = "Sorry you can not inspect this page...";
-
-			function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
-
-			if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
-
-			document.onmousedown = rtclickcheck;
